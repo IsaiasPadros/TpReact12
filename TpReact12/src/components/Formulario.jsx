c7945964e2db2a8a7d07fe6557d7d4dbfce90c8a
unknown <isaiaspadros2@gmail.com>
2023-08-06 14:53:48 -0300

    tercer commit

--- TpReact12/src/components/Formulario.jsx
+++ TpReact12/src/components/Formulario.jsx
@@ -1,37 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import ListaNoticias from "./ListaNoticias";
 
 const Formulario = () => {
-  const [formData, setFormData] = useState({
-    categoria: "",
-    pais: "",
-  });
+  const [categoria, setCategoria] = useState("");
+  const [pais, setPais] = useState("");
   const [noticias, setNoticias] = useState([]);
   const [mostrarNoticias, setMostrarNoticias] = useState(false);
-  const [paises, setPaises] = useState([]);
-  const [categorias, setCategorias] = useState([]);
 
+  // Lista de categorías disponibles
+  const categorias = [
+    { value: "business", label: "Negocios" },
+    { value: "entertainment", label: "Entretenimiento" },
+    { value: "world", label: "Globales" },
+    { value: "health", label: "Salud" },
+    { value: "science", label: "Ciencia" },
+    { value: "sports", label: "Deportes" },
+    { value: "technology", label: "Tecnología" },
+  ];
 
-
-  
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+  // Lista de países disponibles
+  const paises = [
+    { value: "de", label: "Alemania" },
+    { value: "ar", label: "Argentina" },
+    { value: "br", label: "Brasil" },
+    { value: "cl", label: "Chile" },
+    { value: "us", label: "Estados Unidos" },
+    { value: "es", label: "España" },
+    { value: "mx", label: "México" },
+    { value: "jp", label: "Japón" },
+  ];
 
   const consultarAPI = async () => {
     try {
-      const { categoria, pais } = formData;
-      const url = `https://newsdata.io/api/1/news?apikey=pub_27255eeb9aed0b36610336db322bf162164af&category=${categoria}&country=${pais}`;
-
-      const response = await fetch(url);
-      const data = await response.json();
-      setNoticias(data.results);
+      const url = `https://newsdata.io/api/1/news?apikey=pub_24226bea9b87e0d87603664a9762b89a71aa4&category=${categoria}&country=${pais}`;
+      const respuesta = await fetch(url);
+      const dato = await respuesta.json();
+      setNoticias(dato);
       setMostrarNoticias(true);
     } catch (error) {
       console.log(error);
@@ -44,48 +49,40 @@ const Formulario = () => {
   };
 
   return (
-    <Container className="text-center">
-      <Form className="row" onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="tarea">
-          <Form.Label className="my-3">Seleccione su país</Form.Label>
-          <Form.Select
-            name="pais"
-            onChange={handleChange}
-            value={formData.pais}
-          >
-            <option value="">Seleccionar país</option>
-            {paises.map((pais) => (
-              <option key={pais.codigo} value={pais.codigo}>
-                {pais.nombre}
-              </option>
-            ))}
-          </Form.Select>
-          <Form.Label className="my-3">
-            Ingrese una categoría de noticias:
-          </Form.Label>
-          <Form.Select
-            name="categoria"
-            onChange={handleChange}
-            value={formData.categoria}
-          >
-            <option>Elige qué tipo de noticia te interesa</option>
-            {categorias.map((categoria) => (
-              <option key={categoria.id} value={categoria.nombre}>
-                {categoria.nombre}
-              </option>
-            ))}
-          </Form.Select>
-          <Container className="my-3 text-center">
-            <Button variant="success" type="submit">
-              Buscar
-            </Button>
-          </Container>
-        </Form.Group>
-      </Form>
-      <Container className="row">
-        {mostrarNoticias && <ListaNoticias noticias={noticias} />}
+    <>
+      <Container className="text-center">
+        <Form className="row" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="tarea">
+            <Form.Label className="my-3">Seleccione su país</Form.Label>
+            <Form.Select onChange={(e) => setPais(e.target.value)} value={pais}>
+              <option value="">Seleccionar país</option>
+              {paises.map((pais) => (
+                <option key={pais.value} value={pais.value}>
+                  {pais.label}
+                </option>
+              ))}
+            </Form.Select>
+            <Form.Label className="my-3">Ingrese una categoría de noticias:</Form.Label>
+            <Form.Select onChange={(e) => setCategoria(e.target.value)} value={categoria}>
+              <option>Elige qué tipo de noticia te interesa</option>
+              {categorias.map((categoria) => (
+                <option key={categoria.value} value={categoria.value}>
+                  {categoria.label}
+                </option>
+              ))}
+            </Form.Select>
+            <Container className="my-3 text-center">
+              <Button variant="success" type="submit">
+                Buscar
+              </Button>
+            </Container>
+          </Form.Group>
+        </Form>
       </Container>
-    </Container>
+      <Container className="row">
+        {mostrarNoticias && <ListaNoticias noticias={noticias.results} />}
+      </Container>
+    </>
   );
 };
 
